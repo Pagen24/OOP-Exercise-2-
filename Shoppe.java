0e24647fc2a28c39f8e9c64b8cd254201537669c
Pagen24 <123439216+Pagen24@users.noreply.github.com>
2023-01-25 21:27:21 +0800

    Create Shoppe.java

---
+++ Shoppe.java
@@ -0,0 +1,26 @@
+public  abstract class Shopee
+{
+  protected String product_name;
+	protected int price;
+
+	public Shopee(String product_name, int price) {
+		this.product_name=product_name;
+		this.price=price;
+	}
+
+	public abstract String item_info();
+
+	public String getProductName(){
+		return product_name;
+	}
+	public void setProductName(String product_name) {
+		this.product_name=product_name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price=price;
+	}
+}
